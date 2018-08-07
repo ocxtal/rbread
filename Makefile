d508c69286bad73e5643725023fe44203618b7b8
Hajime Suzuki <yline5050hs@gmail.com>
2018-08-07 15:30:29 +0900

    add bz2 and xz (lzma)

--- Makefile
+++ Makefile
@@ -2,7 +2,7 @@
 CC = gcc
 OFLAGS = -O3 -march=native
 CFLAGS = $(OFLAGS) -std=c99 -Wall
-LDFLAGS = -lz
+LDFLAGS = -lz -lbz2
 
 all: zc.c miniz.c miniz_tdef.c miniz_tinfl.c
 	$(CC) $(CFLAGS) -o zc $^ $(LDFLAGS)
---
+++ scat.c
@@ -0,0 +1,55 @@
+
+/**
+ * @file scat.c
+ * @brief minimal zcat
+ *
+ * @author Hajime Suzuki
+ * @date 2018/8/7
+ * @license MIT
+ */
+
+#if defined(__linux__) && !defined(_POSIX_C_SOURCE)
+#  define _POSIX_C_SOURCE		200112L
+#endif
+#if defined(__darwin__) && !defined(_DARWIN_C_FULL)
+#  define _DARWIN_C_SOURCE		_DARWIN_C_FULL
+#endif
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include "zc.h"
+
+int main(int argc, char *argv[])
+{
+	if(argc > 1 && strcmp(argv[1], "-h") == 0) {
+		fprintf(isatty(fileno(stdout)) ? stderr : stdout,
+			"\n"
+			"  zc -- minimal gzcat\n"
+			"    please visit https://github.com/ocxtal/zc for more information\n"
+			"\n"
+		);
+		return(0);
+	}
+
+	int ret = 1;
+	size_t const buf_size = ZC_BUF_SIZE;
+	uint8_t *buf = malloc(buf_size);
+	for(char *const *p = &argv[1]; *p != NULL; p++) {
+		zc_t *zc = zcopen(*p);
+		if(zc == NULL) {
+			fprintf(stderr, "failed to open file `%s'\n", *p);
+			goto _main_error;
+		}
+		while(zceof(zc) == 0) {
+			fwrite(buf, 1, zcread(zc, buf, buf_size), stdout);
+		}
+		zcclose(zc);
+	}
+	ret = 0;
+_main_error:;
+	free(buf);
+	return(ret);
+}
+
+/* end of scat.c */
